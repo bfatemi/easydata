51b8ca2ae767668672dd5d39830a908a72eaa3a2
Bobby Fatemi <Bobby.Fatemi@intusurg.com>
2016-05-18 13:43:40 -0700

    initialized packrat

--- R/easyswap.R
+++ R/easyswap.R
@@ -15,9 +15,8 @@
 #'      is false and the swap is made in memory. See examples below.
 #' @return A modified copy of dt
 #' @family quick wranglers
-#' @example /examples/example-swap.R
+#' @example /examples/example-easyswap.R
 #' @export
-#' 
 easyswap <- function(dt=NULL, find=NA, swap=NULL, cols=NULL, force=FALSE, copy=FALSE){
 
     if(is.null(dt))
