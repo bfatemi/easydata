f1ddcf66dacb3b0322007d0f623853ea120c03e7
Bobby Fatemi <bfatemi07@gmail.com>
2016-10-13 11:27:00 -0700

    added reformit

---
+++ R/reformit.R
@@ -0,0 +1,13 @@
+#' Construct formula
+#' 
+#' A helper to build a formula
+#'
+#' @param lhs Character vector
+#' @param rhs Character vector
+#'
+#' @export
+reformit <- function(lhs, rhs){
+    c.lhs <- paste0(lhs, collapse = " + ")
+    c.rhs <- paste0(rhs, collapse = " + ")
+    stats::as.formula(paste0(c.lhs, " ~ ", c.rhs))
+}
