fd973c7f7f1891d74205056f133855d40add8470
Bobby Fatemi <bfatemi07@gmail.com>
2016-11-10 11:56:44 -0800

    added labeler for regression eq to ggplot

--- development/easygg.R
+++ development/easygg.R
@@ -15,3 +15,15 @@ easytheme <- function(){
 easytitle <- function(main, sub){
     eval(ggtitle(substitute(atop(main, atop(italic(sub), "")))))
 }
+
+lmeqn <- function(m) {
+    l <- list(a = format(coef(m)[1], digits = 2),
+              b = format(abs(coef(m)[2]), digits = 2),
+              r2 = format(summary(m)$r.squared, digits = 3))
+    if (coef(m)[2] >= 0) {
+        eq <- substitute(italic(y) == a+b*italic(log(x))*","~~italic(r)^2~"="~r2,l)
+    }else{
+        eq <- substitute(italic(y) == a - b*italic(log(x))*","~~italic(r)^2~"="~r2,l)
+    }
+    as.character(as.expression(eq))
+}
