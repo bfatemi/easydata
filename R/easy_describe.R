29a8afe855c4b87a19bffd8a6f31a9fd756c1161
Bobby Fatemi <Bobby.Fatemi@intusurg.com>
2017-01-24 14:44:47 -0800

    Fixed bug with pcc when class name is given as a character vector of length > 1

--- R/easy_describe.R
+++ R/easy_describe.R
@@ -63,5 +63,52 @@ easy_describe <- function(DT, cols=NULL, cclass = NULL, FUN = NULL, all=NULL){
 
 globalVariables(c("count_unique", "count_NA", "count_nonNA", "CName", "Class", "range_values", "pct_true"))
 
-
+# easy_describe <- function(DT, cols=NULL, cclass = NULL, FUN = NULL, all=NULL){
+#     
+#     if(!is.data.table(DT)) stop("DT should be data.table class")
+#     
+#     ccdt <- pcc(DT, bret = TRUE) # start the descriptive table
+#     
+#     ## count of unique values
+#     ## count of NAs per columns
+#     ##
+#     cols <- ccdt[, CName]   # get colnames of DT to describe
+#     
+#     ccdt[, count_nonNA   := sapply(cols, function(i) sum(DT[, !is.na(get(i))]))]
+#     ccdt[, count_NA     := nrow(DT) - count_nonNA]
+#     ccdt[, count_unique := sapply(cols, function(i) length(unique(DT[, get(i)])))]
+#     ccdt[, pct_true := NA_real_]
+#     
+#     ## date range of date class columns
+#     ##
+#     dcols <- ccdt[Class == "Date", CName]   # get date columns if any
+#     if(length(dcols) > 0){
+#         for(d in dcols)
+#             ccdt[CName == d, 
+#                  range_values := paste0("(", min(DT[!is.na(get(d)), get(d)]), "):(", max(DT[!is.na(get(d)), get(d)]), ")")]
+#     }
+#     
+#     dcols <- ccdt[Class %in% c("integer", "numeric"), CName]   # get date columns if any
+#     if(length(dcols) > 0){
+#         for(d in dcols)
+#             ccdt[CName == d, 
+#                  range_values := paste0("(", min(DT[!is.na(get(d)), get(d)]), "):(", max(DT[!is.na(get(d)), get(d)]), ")")]
+#     }
+#     
+#     dcols <- ccdt[Class == "logical", CName]   # get date columns if any
+#     if(length(dcols) > 0){
+#         for(d in dcols){
+#             ccdt[CName == d, range_values := "(FALSE):(TRUE)"]
+#             ccdt[CName == d, pct_true := DT[!is.na(get(d)), round(sum(get(d))/count_nonNA, 3)]]
+#         }
+#     }
+#     setnames(ccdt, c("Pos", "CName", "Class"), c("col_position", "col_name", "col_class"))
+#     setcolorder(ccdt, c("col_position", "col_name", "col_class", "count_unique", 
+#                         "count_NA", "count_nonNA", "range_values", "pct_true"))
+#     setorderv(ccdt, c("col_class", "count_unique", "col_position"))
+#     
+#     easydata::pcc(ccdt)
+#     return(ccdt[])
+#     
+# }
 
